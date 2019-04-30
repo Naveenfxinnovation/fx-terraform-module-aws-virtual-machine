5a094423b9bb35d7c54b448f3120b65ab42aaaa2
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2019-04-30 19:37:28 -0400

    test: uncomments variables

--- examples/standard-ec2-with-volume/variables.tf
+++ examples/standard-ec2-with-volume/variables.tf
@@ -1,8 +1,7 @@
-//variable "access_key" {
-//  description = "Credentials: AWS access key."
-//}
-//
-//variable "secret_key" {
-//  description = "Credentials: AWS secret key. Pass this a variable, never write password in the code."
-//}
+variable "access_key" {
+  description = "Credentials: AWS access key."
+}
 
+variable "secret_key" {
+  description = "Credentials: AWS secret key. Pass this a variable, never write password in the code."
+}
