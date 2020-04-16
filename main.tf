1e734d594e61094e0bd2db4892ff70b7b15f06b2
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-04-16 17:35:18 -0400

    feat: handle elastic IPs for EC2 instances

--- main.tf
+++ main.tf
@@ -232,6 +232,27 @@ resource "aws_instance" "this" {
 }
 
 ####
+# Elastic IP
+####
+
+locals {
+  should_create_elastic_ip                              = var.instance_count > 0 && var.eip_create
+  should_create_elastic_ip_for_extra_network_interfaces = var.instance_count > 0 && var.extra_network_interface_eips_count > 0
+
+  network_interface_with_eip_ids = [
+    for i, network_interface in aws_network_interface.this :
+    network_interface.id
+    if element(var.extra_network_interface_eips_enabled, i % var.extra_network_interface_count) == true
+  ]
+}
+
+resource "aws_eip" "this" {
+  count = local.should_create_elastic_ip ? var.instance_count : 0
+
+  vpc = true
+}
+
+####
 # Key Pair
 ####
 
