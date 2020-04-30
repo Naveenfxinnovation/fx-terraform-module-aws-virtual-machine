acda0ee5d478f82f83b3a3b29984d6cd108d87f2
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-04-30 15:41:09 -0400

    test: adds key pair in ASG example

--- examples/autoscaling-group/main.tf
+++ examples/autoscaling-group/main.tf
@@ -102,10 +102,15 @@ module "example" {
 
   root_block_device_volume_size = 8
 
+  key_pair_create     = true
+  key_pair_name       = "tftest${random_string.this.result}"
+  key_pair_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohAK41 email@example.com"
+
   external_volume_count        = 2
   external_volume_sizes        = [5]
   external_volume_device_names = ["/dev/sdh", "/dev/sdi"]
 
+  iam_instance_profile_iam_role_name         = "tftest${random_string.this.result}"
   iam_instance_profile_iam_role_policy_count = 1
   iam_instance_profile_iam_role_policy_arns  = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
 }
