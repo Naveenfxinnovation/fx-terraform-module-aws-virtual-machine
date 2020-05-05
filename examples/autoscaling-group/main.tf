b2ded2e7eaa54a317cd3646bbde30692a8eecaa2
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-05-05 11:20:54 -0400

    Revert "refactor (BREAKING): use `name` for launch template instead of `launch_template_name`"
    This reverts commit 25f2e8666485fca7852fd5ff8bb33cd9d64d2d5a.

--- examples/autoscaling-group/main.tf
+++ examples/autoscaling-group/main.tf
@@ -69,7 +69,7 @@ resource "aws_lb_listener" "example" {
 module "example" {
   source = "../../"
 
-  name = "tftest${random_string.this.result}"
+  name = "tftest-asg"
 
   subnet_ids_count = 2
   subnet_ids       = data.aws_subnet_ids.all.ids
@@ -86,6 +86,8 @@ module "example" {
   }
 
   use_autoscaling_group = true
+
+  launch_template_name = "tftest${random_string.this.result}"
 
   autoscaling_group_max_size          = 2
   autoscaling_group_min_size          = 1
