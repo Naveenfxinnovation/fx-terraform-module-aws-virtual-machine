f77d322dfe0c3516f1674dc026487d0c8aebfa01
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-05-13 17:09:15 -0400

    fix: removes placement_group as it fails for provider 2.61

--- examples/autoscaling-group-windows/main.tf
+++ examples/autoscaling-group-windows/main.tf
@@ -39,21 +39,6 @@ resource "aws_placement_group" "example" {
   strategy = "cluster"
 }
 
-
-resource "aws_lb" "example" {
-  name               = "tftestasg${random_string.this.result}"
-  internal           = true
-  load_balancer_type = "network"
-  subnets            = data.aws_subnet_ids.all.ids
-}
-
-resource "aws_lb_target_group" "example" {
-  name     = "tftestasg${random_string.this.result}"
-  port     = 22
-  protocol = "TCP"
-  vpc_id   = data.aws_vpc.default.id
-}
-
 module "example" {
   source = "../../"
 
@@ -62,7 +47,7 @@ module "example" {
   subnet_ids_count = 2
   subnet_ids       = data.aws_subnet_ids.all.ids
   ami              = data.aws_ami.windows_2019.image_id
-  instance_type    = "t3.large"
+  instance_type    = "m5.large"
 
   tags = {
     Example = "TFTEST example"
@@ -81,15 +66,16 @@ module "example" {
   autoscaling_group_max_size          = 2
   autoscaling_group_min_size          = 1
   autoscaling_group_name              = "tftestasg${random_string.this.result}"
-  autoscaling_group_health_check_type = "ELB"
-  autoscaling_group_target_group_arns = [aws_lb_target_group.example.arn]
+  autoscaling_group_health_check_type = "EC2"
   autoscaling_group_tags = {
     ASGName = "tftestasg${random_string.this.result}"
   }
   autoscaling_group_wait_for_capacity_timeout = "15m"
   autoscaling_group_wait_for_elb_capacity     = 1
 
-  placement_group = "tftest{random_string.this.result}"
+  // Does not work with Provider AWS 2.61: https://github.com/terraform-providers/terraform-provider-aws/issues/13236
+  // Uncomment once Provider 2.62 is out
+  //  placement_group = "tftest{random_string.this.result}"
 
   root_block_device_volume_size = 8
 }
