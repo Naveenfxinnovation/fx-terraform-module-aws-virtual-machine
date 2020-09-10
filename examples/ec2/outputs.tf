2a16f3ac3f00d7411c605252b1e1c01d09685d2c
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-09-10 13:34:29 -0400

    Revert "fix: comment outputs until upstream bug is fixed"
    This reverts commit f9684a8c31ba640f6c05fb68e604f484dd208fe1.

--- examples/ec2/outputs.tf
+++ examples/ec2/outputs.tf
@@ -1,18 +1,18 @@
-//#####
-//# EC2 with options
-//#####
-//
-//output "options" {
-//  value = module.options
-//}
-//
-//#####
-//# EC2 with extra volumes
-//#####
-//
-//output "with_volumes" {
-//  value = module.with_volumes
-//}
+#####
+# EC2 with options
+#####
+
+output "options" {
+  value = module.options
+}
+
+#####
+# EC2 with extra volumes
+#####
+
+output "with_volumes" {
+  value = module.with_volumes
+}
 
 #####
 # EC2 with extra NICs & EIP
@@ -21,11 +21,11 @@
 output "with_nic_and_eips" {
   value = module.with_nic_and_eips
 }
-//
-//#####
-//# EC2 using external resources
-//#####
-//
-//output "externals" {
-//  value = module.externals
-//}
+
+#####
+# EC2 using external resources
+#####
+
+output "externals" {
+  value = module.externals
+}
